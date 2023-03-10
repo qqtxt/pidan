c0328c5cee56d0551bad5359344f9463a03ada4a
zhh2100 <112738102@qq.com>
2023-03-11 01:50:48 +0800

    go on

--- app/AppService.php
+++ app/AppService.php
@@ -11,16 +11,16 @@ class AppService extends Service
 	public $bind = [//只能在此bind
 		//'request'                 => Request::class,
 	];
-    public function register()
-    {
-        // 服务注册
-        $app=app();
-    	$type=$app->config->get('session.type');
-        $app->bind('session','pidan\\session\\' . Str::studly($type));
-    }
+	public function register()
+	{
+		// 服务注册
+		$app=app();
+		$type=$app->config->get('session.type');
+		$app->bind('session','pidan\\session\\' . Str::studly($type));
+	}
 
-    public function boot()
-    {
-        //echo 'AppService_boot<br>';// 服务启动
-    }
+	public function boot()
+	{
+		//echo 'AppService_boot<br>';// 服务启动
+	}
 }
